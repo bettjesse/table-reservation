9924940c8086568ea7ebb3a04adcf2a553df0eff
Jesee Bett <98686980+bettjesse@users.noreply.github.com>
2024-12-18 09:59:08 +0300

    Update Succes.jsx

--- client/pepper-tree/src/components/Succes.jsx
+++ client/pepper-tree/src/components/Succes.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { TiTick, TiTimes } from 'react-icons/ti';
 
 import FeedBack from './FeedBack';
-import  {Link} from  "react-router-dom"
+import { Link } from 'react-router-dom';
 
 const Success = () => {
   const [showFeedbackModal, setShowFeedbackModal] = useState(false);
@@ -13,10 +13,6 @@ const Success = () => {
 
   const handleCloseFeedback = () => {
     setShowFeedbackModal(false);
-  };
-
-  const handleGoBackHome = () => {
-    // Logic to navigate back to the home page goes here
   };
 
   return (
@@ -31,22 +27,18 @@ const Success = () => {
             Thank you for your reservation. You will receive an email shortly to confirm your booking.
           </p>
           <div className="mt-4 space-x-2 flex">
-
-          <button
-              className="bg-blue-500 text-white px-4 py-2 text-sm rounded"
+            <button
+              className="bg-gradient-to-r from-blue-500 to-blue-700 text-white px-6 py-2 text-sm rounded-lg shadow-lg flex items-center gap-2 hover:from-blue-600 hover:to-blue-800 transform hover:scale-105 transition duration-300 ease-in-out"
               onClick={handleOpenFeedback}
             >
+              <TiTick size={20} className="text-white" />
               We'd Love to Hear from You
             </button>
-            <Link to = "/">
-            <p
-              className=" text-gray-900 px-4  mr-2 md:mr-0 md:text-xl underline py-2 "
-             
-            >
-               Return to Home page
-            </p>
+            <Link to="/">
+              <p className="text-gray-900 px-4 mr-2 md:mr-0 md:text-xl underline py-2">
+                Return to Home page
+              </p>
             </Link>
-           
           </div>
         </div>
       </div>
@@ -55,11 +47,13 @@ const Success = () => {
       {showFeedbackModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white relative p-4 rounded-lg shadow-lg">
-          <span className="absolute top-2 right-2 cursor-pointer text-gray-600" onClick={handleCloseFeedback}>
-  <TiTimes size={40} />
-</span>
-
-            <FeedBack /> {/* Render the Feedback component */}
+            <span
+              className="absolute top-2 right-2 cursor-pointer text-gray-600"
+              onClick={handleCloseFeedback}
+            >
+              <TiTimes size={40} />
+            </span>
+            <FeedBack />
           </div>
         </div>
       )}
